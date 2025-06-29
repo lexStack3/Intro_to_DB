3a926ba20675347d0c7026e78daffc9cb5d5635a
lexStack3 <alexanderedim80@gmail.com>
2025-06-30 00:16:04 +0100

    Prints the description of the Books table from the alx_book_store database

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,4 @@
 -- Prints the full description of the Books table from the alx_book_store database.
-USE alx_book_store;
-DESC Books;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA, COLUMN_DEFAULT
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
