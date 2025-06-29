919e2916c9f0fa3fe7af25be25c2c6ac38253f77
lexStack3 <alexanderedim80@gmail.com>
2025-06-30 00:21:08 +0100

    Inserts a single row in the Customers table of the alx_book_store database

---
+++ task_5.sql
@@ -0,0 +1,4 @@
+-- Inserts a single row in the Customer tabke of the alx_book_store.
+USE alx_book_store;
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, "Cole Baidoo", "cbaidoo@sandtech,com", "123 Happiness Ave.");
