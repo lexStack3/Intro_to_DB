9238d99ab050f165794c25abd2ab9e0c62f46ba7
lexStack3 <alexanderedim80@gmail.com>
2025-06-30 00:37:30 +0100

    Create the tables: books, authors, customers, orders, order_details

--- task_2.sql
+++ task_2.sql
@@ -1,23 +1,26 @@
 -- Creates a table `alx_book_store`
 USE alx_book_store;
 
-CREATE TABLE Authors (
+CREATE TABLE authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 -- Books: Stores information about books available in the bookstore.
-CREATE TABLE Books (
+CREATE TABLE books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id) ON DELETE CASCADE
+    CONSTRAINT fk_books_author
+        FOREIGN KEY (author_id)
+        REFERENCES authors(author_id)
+        ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
 -- Customers Table
-CREATE TABLE Customers (
+CREATE TABLE customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -25,19 +28,28 @@ CREATE TABLE Customers (
 );
 
 -- Orders Table
-CREATE TABLE Orders (
+CREATE TABLE orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE
+    CONSTRAINT fk_order_customer
+        FOREIGN KEY (customer_id)
+        REFERENCES customers(customer_id)
+        ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
 -- Order_Details Table
-CREATE TABLE Order_Detail (
+CREATE TABLE order_detail (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
-    FOREIGN KEY (book_id) REFERENCES Books(book_id) ON DELETE CASCADE
+    CONSTRAINT fk_orderdetail_order
+        FOREIGN KEY (order_id)
+        REFERENCES orders(order_id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_orderdetail_book
+        FOREIGN KEY (book_id)
+        REFERENCES books(book_id)
+        ON DELETE CASCADE
 ) ENGINE=InnoDB;
